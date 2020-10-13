9dc10eb6c4577bea9482b6dfeeff6f729559dcba
JP <jpjones22@gmail.com>
2020-10-12 17:22:28 -0700

    Added functionality to select three random objects with conditions.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 'use strict';
 var numberRounds = 25;
 var allProducts = [];
-var prevImageDisplay = []
+var prevImageDisplay = [];
+var selectedImages = [];
 
 function Product(fileName, filePath) {
   this.title = fileName;
@@ -9,7 +10,7 @@ function Product(fileName, filePath) {
   this.alt = fileName;
   this.numberClicks = 0;
   this.numberDisplayed = 0;
-  allProducts.push(allProducts);
+  allProducts.push(this);
 }
 
 function generateRandomNumber(max) {
@@ -21,44 +22,58 @@ function generateRandomNumber(max) {
 /// Cannot have any repeated.
 /// RESULT: Three images that can be displayed.
 
-function generateThreeImages() {
-  var selected = [];
+function generateThreeImages() { //You will generate three different index numbers for me
+  prevImageDisplay = selectedImages;
+  selectedImages = [];
+  var areConditionsTrue;
   for (var i = 0; i < 3; i++) { //This loop will make three choices.
+    areConditionsTrue = false;
+    while (!areConditionsTrue) {
 
-    while (selected.includes(randomImageIndex) && prevImageDisplay.includes(randomImageIndex)) {
       ///keep looping if:
       /// prevSelected[] has one of the random number in it
       /// also if the prevImageDisplay[] has one of the numbers in it.
       var randomImageIndex = generateRandomNumber(allProducts.length);
+      if (selectedImages.includes(randomImageIndex) && prevImageDisplay.includes(randomImageIndex)) {
+        randomImageIndex = generateRandomNumber(allProducts.length);
+      } else {
+        areConditionsTrue = true;
+      }
     }
-    selected.push(randomImageIndex); //add the image index to one of the three to be used
+    selectedImages.push(randomImageIndex); //add the image index to one of the three to be used    
   }
-  console.log(selected, prevImageDisplay); //See what's up
 }
+
+function displayImages() {
+  var imgHTMLElements = [document.getElementById('image-1'), document.getElementById('image-2'), document.getElementById('image-3')];
+  for (var i = 0; i < selectedImages.length; i++) {
+    imgHTMLElements[i].src = allProducts[selectedImages[i]].filePath;
+
+    debugger;
+  }
+}
+
 new Product('bathroom', 'img/bathroom.jpg'); // fileName, filePath
-new Product('bubblegum','img/bubblegum');
-new Product('dog-duck','img/dog-duck.jpg');
-new Product('pet-sweep','img/pet-sweep.jpg');
-new Product('sweep','img/sweep.png');
-new Product('usb','img/usb.gif');
-new Product('wireframe','img/wireframe.png');
-new Product('bag','img/bag.jpg');
-new Product('boots','img/boots.jpg');
-new Product('chair','img/chair.jpg');
-new Product('dragon','img/dragon.jpg');
-new Product('scissors','img/scissors.jpg');
-new Product('tauntaun','img/tauntaun.jpg');
-new Product('water-can','img/water-can.jpg');
-new Product('banana','img/banana.jpg');
-new Product('breakfast','img/breakfast.jpg');
-new Product('cthulhu','img/cthulhu.jpg');
-new Product('pen','img/pen.jpg');
-new Product('shark','img/shark.jpg');
-new Product('unicorn','img/unicorn.jpg');
-
-
-
-
-
-
+new Product('bubblegum', 'img/bubblegum');
+new Product('dog-duck', 'img/dog-duck.jpg');
+new Product('pet-sweep', 'img/pet-sweep.jpg');
+new Product('sweep', 'img/sweep.png');
+new Product('usb', 'img/usb.gif');
+new Product('bag', 'img/bag.jpg');
+new Product('boots', 'img/boots.jpg');
+new Product('chair', 'img/chair.jpg');
+new Product('dragon', 'img/dragon.jpg');
+new Product('scissors', 'img/scissors.jpg');
+new Product('tauntaun', 'img/tauntaun.jpg');
+new Product('water-can', 'img/water-can.jpg');
+new Product('banana', 'img/banana.jpg');
+new Product('breakfast', 'img/breakfast.jpg');
+new Product('cthulhu', 'img/cthulhu.jpg');
+new Product('pen', 'img/pen.jpg');
+new Product('shark', 'img/shark.jpg');
+new Product('unicorn', 'img/unicorn.jpg');
+new Product('wine-glass', 'img/wine-glass.jpg');
+debugger;
 generateThreeImages();
+displayImages();
+
