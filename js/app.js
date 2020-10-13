54c3d87a8dd8f07a8ab2ac7436a4ed77c18ee2c3
JP <jpjones22@gmail.com>
2020-10-12 20:01:29 -0700

    added image change. Tracks view and clicks.  displayed final results after rounds are over

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 'use strict';
-var numberRounds = 25;
+var numberRounds = 2;
+var timesVoted = 0;
 var allProducts = [];
 var prevImageDisplay = [];
 var selectedImages = [];
-
+var imageContainer = document.getElementById('image-display');
 function Product(fileName, filePath) {
-  this.title = fileName;
+  this.titleAlt = fileName;
   this.filePath = filePath;
-  this.alt = fileName;
   this.numberClicks = 0;
   this.numberDisplayed = 0;
   allProducts.push(this);
@@ -29,32 +29,75 @@ function generateThreeImages() { //You will generate three different index numbe
   for (var i = 0; i < 3; i++) { //This loop will make three choices.
     areConditionsTrue = false;
     while (!areConditionsTrue) {
-
       ///keep looping if:
-      /// prevSelected[] has one of the random number in it
+      /// selectedImages one of the random numbers in it
       /// also if the prevImageDisplay[] has one of the numbers in it.
       var randomImageIndex = generateRandomNumber(allProducts.length);
-      if (selectedImages.includes(randomImageIndex) && prevImageDisplay.includes(randomImageIndex)) {
+
+      if (selectedImages.includes(randomImageIndex) || prevImageDisplay.includes(randomImageIndex)) {
         randomImageIndex = generateRandomNumber(allProducts.length);
       } else {
         areConditionsTrue = true;
       }
+
     }
-    selectedImages.push(randomImageIndex); //add the image index to one of the three to be used    
+
+    selectedImages.push(randomImageIndex); //add the image index be to one of the three to be used    
+    updateNumberDisplayed(randomImageIndex);
   }
+}
+function updateNumberDisplayed(indexNum) {
+  allProducts[indexNum].numberDisplayed++
+
 }
 
 function displayImages() {
   var imgHTMLElements = [document.getElementById('image-1'), document.getElementById('image-2'), document.getElementById('image-3')];
   for (var i = 0; i < selectedImages.length; i++) {
     imgHTMLElements[i].src = allProducts[selectedImages[i]].filePath;
-
-    debugger;
+    imgHTMLElements[i].alt = allProducts[selectedImages[i]].titleAlt;
+    imgHTMLElements[i].title = allProducts[selectedImages[i]].titleAlt;
   }
 }
 
+function displayResults() { //Displays the final results after all rounds are complete
+  var attachMain = document.getElementById('results-display');
+  debugger;
+  console.log(attachMain);
+  var getP;
+  for (var i = 0; i < allProducts.length; i++) {
+    getP = document.createElement('h4');
+    getP.innerText = `${allProducts[i].titleAlt} had ${allProducts[i].numberClicks} votes, and was seen ${allProducts[i].numberDisplayed} times`;
+
+    attachMain.appendChild(getP);
+  }
+
+}
+
+function clickHandler(event) {
+  //Determine which product has the title of the event that was clicked.
+  //search through each product until found, if not throw new error
+  console.log('allProducts: ', allProducts.length);
+  for (var i = 0; i < allProducts.length; i++) {  //Searches for the matching target title to titleAlt
+    if (event.target.title === allProducts[i].titleAlt) {  //if you find it, update the clicks and break the loop
+      allProducts[i].numberClicks++;
+      break;
+    }
+
+  }
+  timesVoted++;
+  console.log(timesVoted);
+  if (timesVoted > numberRounds) {  //If we exceed the number of authorized click, disable the event listener
+    imageContainer.removeEventListener('click', clickHandler);
+    displayResults();
+  }
+  else {
+    generateThreeImages();
+    displayImages();
+  }
+}
 new Product('bathroom', 'img/bathroom.jpg'); // fileName, filePath
-new Product('bubblegum', 'img/bubblegum');
+new Product('bubblegum', 'img/bubblegum.jpg');
 new Product('dog-duck', 'img/dog-duck.jpg');
 new Product('pet-sweep', 'img/pet-sweep.jpg');
 new Product('sweep', 'img/sweep.png');
@@ -73,7 +116,10 @@ new Product('pen', 'img/pen.jpg');
 new Product('shark', 'img/shark.jpg');
 new Product('unicorn', 'img/unicorn.jpg');
 new Product('wine-glass', 'img/wine-glass.jpg');
-debugger;
+
 generateThreeImages();
 displayImages();
+
+imageContainer.addEventListener('click', clickHandler);
+
 
